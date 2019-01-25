da2e7a79be1b934d8c1d95b0ec8dae8ceb3e26ab
Nik B <nikolaus.bertino@gmail.com>
2019-01-24 20:55:37 -0500

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-freevalue=$1;
+freevalue=$(free | grep -i mem | awk '{print $4}');
 param2=$2;
 
 if [ $freevalue -lt $param2 ]; then
         echo 'warning';
 fi;
 #usage: compare free memory to some value
-#./s.sh $(free | grep -i mem | awk '{print $4}') 110000000
+#./s.sh [some meaningless number] 110000000
